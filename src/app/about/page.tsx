38472373173583459febb0e544a7ecd755f4571e
Carlos Escudero <carlos.escudero.corpas@gmail.com>
2024-09-05 16:12:23 -0500

    responsive home page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/utils/cn";
 
 export default function page() {
   return (
-    <main className={cn("flex flex-1 flex-col px-24 py-12")}>
+    <main className={cn("flex flex-1 flex-col px-8 py-6", "lg:px-24 lg:py-12")}>
       <h1 className="text-center text-5xl font-bold">Learn more about me</h1>
       <ExperienceSection />
       <hr />
